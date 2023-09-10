964c77de0ba0657d5b7d2c37b66c981c84d83ff2
Aldess <106018979+AldessScratch@users.noreply.github.com>
2023-09-10 19:52:51 +1000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const body = document.querySelector('body'),
       sidebar = body.querySelector('nav'),
       toggle = body.querySelector(".toggle"),
       modeSwitch = body.querySelector(".toggle-switch");
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
 checktheusername()
 
@@ -10,36 +11,12 @@ function checktheusername(){
     if (localStorage.getItem('nickname')===null){
         localStorage.setItem('nickname', "Guest")
         document.getElementById("username").innerHTML = "Guest"
-    }else{
-        document.getElementById("username").innerHTML = localStorage.getItem('nickname')
-        if (localStorage.getItem('nickname')==="AldessSc"){
-            document.getElementById("grade").innerHTML = "ADMIN"
-        }
-        else{
-            document.getElementById("username").innerHTML = localStorage.getItem('nickname')
-            if (localStorage.getItem('nickname')==="Arrox78"){
-                document.getElementById("grade").innerHTML = "ADMIN"
-            }
-            else{
-                document.getElementById("username").innerHTML = localStorage.getItem('nickname')
-                if (localStorage.getItem('nickname')==="Asicalug"){
-                    document.getElementById("grade").innerHTML = "STAFF"
-                }
-                else{
-                    document.getElementById("username").innerHTML = localStorage.getItem('nickname')
-                    if (localStorage.getItem('nickname')==="Pix4rt"){
-                        document.getElementById("grade").innerHTML = "STAFF"
-                    }
-                    
-                }
-                
-            }
-            
-        }
-        
     }
 }
 
+if (localStorage.getItem('searchengine')===null){
+    localStorage.setItem('searchengine', 'https://google.com/search?q=%s')
+  }
 function setusername(){
     let result = prompt("Entrez votre nom d'utilisateur, ce nom sera utilisé dans le tchat. Veuillez éviter les caractères spéciaux")
         if (result===null){
@@ -49,3 +26,19 @@ function setusername(){
         }
         
 }
+function switchTheme(e) {
+    if (e.target.checked) {
+        localStorage.setItem('searchmode', 'proxy');
+    }
+    else {       
+          localStorage.setItem('searchmode', 'normal');
+    }    
+}
+if ((localStorage.getItem('searchmode')) === null) {
+    localStorage.setItem('searchmode', 'proxy')
+  }
+  if ((localStorage.getItem('searchmode')) === 'proxy') {
+      toggleSwitch.checked = true;
+  }
+
+  toggleSwitch.addEventListener('change', switchTheme, false);
