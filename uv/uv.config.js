55fd8d5ddde3aeabf0095152097cae1c075ac1cc
Aldess <106018979+AldessScratch@users.noreply.github.com>
2023-10-14 08:38:19 +0200

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://ngabc321.hehemaths.gq/bare/',
+    bare: 'https://then-newt-gtchrb.koyeb.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
