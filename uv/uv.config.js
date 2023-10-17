0a130eb2bd2a62d26923585e6b8278e1432be84d
Aldess <106018979+AldessScratch@users.noreply.github.com>
2023-10-17 16:49:13 +0200

    Add files via upload

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://then-newt-gtchrb.koyeb.app/bare/',
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
