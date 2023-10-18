55cb51e817927c7f0a33230c707714a9662e412c
Aldess <106018979+AldessScratch@users.noreply.github.com>
2023-10-18 07:39:36 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://stem.sciencevictory.org/outerspace/',
+    bare: 'https://tr0x1-math.hop.sh/outerspace/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
