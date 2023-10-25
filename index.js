c8f41b4a768200191d253ec7da17c57ca7e43d44
Aldess <106018979+AldessScratch@users.noreply.github.com>
2023-10-25 15:54:28 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 
-if (localStorage.getItem('searchmode')==="proxy"){
+
   form.addEventListener('submit', async event => {
     event.preventDefault();
     window.navigator.serviceWorker.register('./sw.js', {
@@ -12,25 +12,17 @@ if (localStorage.getItem('searchmode')==="proxy"){
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
 
 
-        localStorage.setItem('iframeurl', __uv$config.prefix + __uv$config.encodeUrl(url))
+        
+          if (localStorage.getItem('searchmode')==="proxy"){
+            localStorage.setItem('iframeurl', __uv$config.prefix + __uv$config.encodeUrl(url))
         localStorage.setItem('staturl', './iframe.html');localStorage.setItem('image', '');localStorage.setItem('appname', 'Recherche')
           window.location.href = "./iframe.html"
+          }
+          else{
+            window.open(url)
+          }
     });
-});
-}else{
-  form.addEventListener('submit', async event => {
-    let url = input.value.trim();
-        if (!isUrl(url)) url = localStorage.getItem('searchengine') + url;
-        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-    window.open(url)
-    
-})}
-
-
-function isUrl(val = '') {
-    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
-    return false;
-};
+  });
 
 function openURL(appname, image, url) {
   localStorage.setItem('staturl', './iframe.html');
