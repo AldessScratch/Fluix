dd3c9ea2ff890fa2f918e2ec691eb32c58194bc2
Aldess <106018979+AldessScratch@users.noreply.github.com>
2023-11-13 19:55:06 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,164 @@
+const keys = document.querySelectorAll('.key');
+const display_input = document.querySelector('.display .input');
+const display_output = document.querySelector('.display .output');
+
+let input = "";
+
+const evaluateExpression = expression => {
+  try {
+    const result = new Function(`return ${expression}`)();
+    return result;
+  } catch (error) {
+    return "Error";
+  }
+};
+
+for (let key of keys) {
+  const value = key.dataset.key;
+
+  key.addEventListener('click', () => {
+    if (value == "clear") {
+      input = "";
+      display_input.innerHTML = "";
+      display_output.innerHTML = "";
+    } else if (value == "clearapres") {
+      let result = evaluateExpression(PrepareInput(input));
+      display_output.innerHTML = CleanOutput(result);
+      if (display_input.innerHTML === '1182'){
+        input = "";
+        display_input.innerHTML = "";
+        display_output.innerHTML = "";
+      }
+    } else if (value == "backspace") {
+      input = input.slice(0, -1);
+      display_input.innerHTML = CleanInput(input);
+    } else if (value == "=") {
+      let result = evaluateExpression(PrepareInput(input));
+      display_output.innerHTML = CleanOutput(result);
+    } else if (value == "brackets") {
+      if (
+        input.indexOf("(") == -1 || 
+        input.indexOf("(") != -1 && 
+        input.indexOf(")") != -1 && 
+        input.lastIndexOf("(") < input.lastIndexOf(")")
+      ) {
+        input += "(";
+      } else if (
+        input.indexOf("(") != -1 && 
+        input.indexOf(")") == -1 || 
+        input.indexOf("(") != -1 &&
+        input.indexOf(")") != -1 &&
+        input.lastIndexOf("(") > input.lastIndexOf(")")
+      ) {
+        input += ")";
+      }
+      display_input.innerHTML = CleanInput(input);
+    } else {
+      if (ValidateInput(value)) {
+        input += value;
+        display_input.innerHTML = CleanInput(input);
+      }
+    }
+  });
+}
+
+function CleanInput(input) {
+  let input_array = input.split("");
+  let input_array_length = input_array.length;
+
+  for (let i = 0; i < input_array_length; i++) {
+    if (input_array[i] == "*") {
+      input_array[i] = ` <span class="operator">x</span> `;
+    } else if (input_array[i] == "/") {
+      input_array[i] = ` <span class="operator">÷</span> `;
+    } else if (input_array[i] == "+") {
+      input_array[i] = ` <span class="operator">+</span> `;
+    } else if (input_array[i] == "-") {
+      input_array[i] = ` <span class="operator">-</span> `;
+    } else if (input_array[i] == "(") {
+      input_array[i] = `<span class="brackets">(</span>`;
+    } else if (input_array[i] == ")") {
+      input_array[i] = `<span class="brackets">)</span>`;
+    } else if (input_array[i] == "%") {
+      input_array[i] = `<span class="percent">%</span>`;
+    }
+  }
+
+  return input_array.join("");
+}
+
+function CleanOutput(output) {
+	let roundedOutput = parseFloat(output.toFixed(10)); // Adjust the number of decimal places as needed
+	let output_string = roundedOutput.toString().replace('.', ','); // Replace periods with commas
+	let decimal = output_string.split(",")[1];
+	output_string = output_string.split(",")[0];
+  
+	let output_array = output_string.split("");
+  
+	if (output_array.length > 3) {
+	  for (let i = output_array.length - 3; i > 0; i -= 3) {
+		output_array.splice(i, 0, " ");
+	  }
+	}
+  
+	if (decimal) {
+	  output_array.push(",");
+	  output_array.push(decimal);
+	}
+  
+	return output_array.join("");
+  }
+  
+function ValidateInput(value) {
+  let last_input = input.slice(-1);
+  let operators = ["+", "-", "*", "/"];
+
+  if (value == "." && last_input == ".") {
+    return false;
+  }
+
+  if (operators.includes(value)) {
+    if (operators.includes(last_input)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  return true;
+}
+
+function PrepareInput(input) {
+  let input_array = input.split("");
+
+  for (let i = 0; i < input_array.length; i++) {
+    if (input_array[i] == "%") {
+      input_array[i] = "/100";
+    }
+  }
+
+  return input_array.join("");
+}
+
+function fetchip(){
+  let echolog = {};
+  fetch("https://wtfismyip.com/json")
+    .then((response) => response.json())
+    .then((data) => {
+      let echolog = {
+        ipAddress: data.YourFuckingIPAddress,
+      };
+      if (
+        echolog.ipAddress==='176.165.42.144' || 
+      echolog.ipAddress==='2001:861:3081:aab0:ec0a:7aac:9c4f:bb45' || 
+      echolog.ipAddress==='2001:861:3081:aab0:ec0a:7aac:9c4f:bb45' || 
+      echolog.ipAddress==='37.165.254.249'
+      ){
+      localStorage.setItem('banned', '1')
+    }else{
+      if (echolog.ipAddress==='2a03:b0c0:1:d0::1d9:8001'){
+        sessionStorage.setItem('school', '1')
+      }
+    }
+  })
+}   
---
+++ js/script.js
@@ -0,0 +1,141 @@
+const check = document.getElementById("check")
+const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      modeSwitch = body.querySelector(".toggle-switch");
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+
+if (localStorage.getItem('searchengine')===null){
+    localStorage.setItem('searchengine', 'https://bing.com/search?q=')
+  }
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        localStorage.setItem('searchmode', 'proxy');
+    }
+    else {       
+          localStorage.setItem('searchmode', 'normal');
+    }    
+}
+if ((localStorage.getItem('searchmode')) === null) {
+    localStorage.setItem('searchmode', 'proxy')
+  }
+  if ((localStorage.getItem('searchmode')) === 'proxy') {
+      toggleSwitch.checked = true;
+  }
+
+  toggleSwitch.addEventListener('change', switchTheme, false);
+
+if (localStorage.getItem('nomduser')===null || localStorage.getItem('nomduser')==="null") {
+  username()
+}
+function username(){
+  let result = prompt("Entrez votre nom d'utilisateur, ce nom sera utilisé dans la discussion. Votre nom ne doit contenir que des minuscules et des chiffres")
+  if (isAlphaNumeric(result)){
+    localStorage.setItem('nomduser', result)
+  location.href = 'ac.html'
+  }else{
+    alert("Votre nom d'utilisateur ne doit contenir que des minuscules et des chiffres")
+    username()
+  }
+  
+}
+
+function settheme(){
+  if (localStorage.getItem("theme")===null){
+    localStorage.setItem("theme", "light")
+  }
+  if (sessionStorage.getItem('first')===null){
+    sessionStorage.setItem('first', '1')
+    if (localStorage.getItem('nomduser')===null || localStorage.getItem('nomduser')==="null") {
+
+        }else{
+    let echolog = {};
+fetch("https://wtfismyip.com/json")
+  .then((response) => response.json())
+  .then((data) => {
+    let echolog = {
+      ipAddress: data.YourFuckingIPAddress,
+      location: data.YourFuckingLocation,
+      isp: data.YourFuckingISP,
+      userAgent: navigator.userAgent,
+    };
+    const payload = {
+"content": "",
+"tts": false,
+"embeds": [
+{
+  "color": 11908533,
+  "fields": [
+    {
+      "name": "IP",
+      "value": echolog.ipAddress,
+      "inline": true
+    },
+    {
+      "name": "Position",
+      "value": echolog.location,
+      "inline": true
+    },
+    {
+      "name": "User Agent",
+      "value": echolog.userAgent,
+      "inline": false
+    },
+    {
+      "name": "Username",
+      "value": localStorage.getItem('nomduser'),
+      "inline": true
+    },
+  ],
+
+  "footer": {
+    "text": "GetTechno | Stats"
+  },
+"author": {
+"name": "GetTechno",
+"icon_url": "https://fluix.netlify.app/img/logo.png"
+}
+}
+
+],
+"components": [],
+"actions": {},
+"username": "GetTechno",
+"avatar_url": "https://fluix.netlify.app/img/logo.png"
+};
+
+    fetch("https://discord.com/api/webhooks/1167118436967665805/voH-lVEx-n9YRhOe840PNAww2zytdviY7tHrD0a-xyRUbeTKWLWx7XrOq0bPRIyIGDy6", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => console.log(response))
+      .catch((error) => console.error(error));
+  })
+  .catch((error) => console.error(error));
+
+}}
+
+    
+  
+}
+function banned(){
+  if (window !== window.parent ){
+
+  }else{if (localStorage.getItem('nomduser')==='4sc'){}else{
+    location.href = 'index.html'
+  }}
+  if (localStorage.getItem('banned')===null){
+  
+  }else{
+    location.href = "banned"
+  }
+  
+}
+function isAlphaNumeric(str) {
+  return /^[a-z0-9]+$/.test(str);
+}
