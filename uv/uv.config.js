5c8bbe1b63637dbfbac944cd20d4e3f6c8e7cb8d
Aldess <106018979+AldessScratch@users.noreply.github.com>
2023-11-18 09:44:46 +0100

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://mathproblemsstepbystep.vercel.app/outerspace/',
+    bare: 'https://shuttle.lolmath.tk/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
